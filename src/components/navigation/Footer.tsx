26901cbd04fed93f88e3d816dc99fa75572ecd28
arielahr <arielah1997@gmail.com>
2022-07-20 21:06:29 +0300

    added complete footer

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import whiteLogo from '../../assets/images/logo-white.svg'
+import links from './links'
+
+// Social media imports
+import facebook from '../../assets/images/icon-facebook.svg'
+import twitter from '../../assets/images/icon-twitter.svg'
+import pinterest from '../../assets/images/icon-pinterest.svg'
+import instagram from '../../assets/images/icon-instagram.svg'
 
 const Footer: React.FC = () => {
     return (
         <footer className='footer-wrapper'>
-            <div>
-                asd
+            <div className='footer-logo'>
+                <img src={whiteLogo} alt="white company logo" />
+            </div>
+            <div className='footer-links'>
+                {/* TODO: 3 lists of links */}
+                {links.map((item, index) => (
+                    <ul className='link-wrap' key={index}>
+                        <h5>{item.title}</h5>
+                        {item.sublinks.map(link => <li><a href={link.href}>{link.title}</a></li>)}
+                    </ul>
+                ))}
+            </div>
+            <div className='footer-social-media'>
+                <img src={facebook} alt="social media icon" />
+                <img src={twitter} alt="social media icon" />
+                <img src={pinterest} alt="social media icon" />
+                <img src={instagram} alt="social media icon" />
             </div>
         </footer>
     )
---
+++ src/components/navigation/links.js
@@ -0,0 +1,53 @@
+module.exports = [
+    {
+        title: 'Features',
+        sublinks: [
+            {
+                title: 'Link Shortening',
+                href: '#',
+            },
+            {
+                title: 'Branded Links',
+                href: '#',
+            },
+            {
+                title: 'Analytics',
+                href: '#',
+            },
+        ],
+    },
+    {
+        title: 'Resources',
+        sublinks: [
+            {
+                title: 'Blog',
+                href: '#',
+            },
+            {
+                title: 'Developers',
+                href: '#',
+            },
+            {
+                title: 'Support',
+                href: '#',
+            },
+        ],
+    },
+    {
+        title: 'Company',
+        sublinks: [
+            {
+                title: 'About',
+                href: '#',
+            },
+            {
+                title: 'Our Team',
+                href: '#',
+            },
+            {
+                title: 'Careers',
+                href: '#',
+            },
+        ],
+    },
+]
