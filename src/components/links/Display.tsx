86d7cc8b8d53357089d544f90ae2dd9e049763b8
arielahr <arielah1997@gmail.com>
2022-07-21 11:04:22 +0300

    fixed margins & paddings

--- src/components/links/Display.tsx
+++ src/components/links/Display.tsx
@@ -33,18 +33,20 @@ const DisplayURL: React.FC<Props> = ({ urls, refresh }) => {
         <>
             {success && <Modal setter={unsuccess} type="success" msg={"Copied link successfuly to clipboard!"} />}
             <p className='display-clear-data' onClick={() => { clearData(); refresh() }}>Clear All Data</p>
-            {urls.map((url: ShortenedURL, index) => (
-                <div className='display-box' key={index}>
-                    <ul>
-                        <li><a href={url.original}>{url.original}</a></li>
-                        <hr className='hrule' />
-                        <div className='flex-dir-row display-bundle'>
-                            <li className='display-short'><a href={url.shortened}>{url.shortened}</a></li>
-                            <li><button className='btn input-btn btn-copy' onClick={(e) => copyToClipboard(url.shortened, e)}>Copy</button></li>
-                        </div>
-                    </ul>
-                </div>
-            ))}
+            <div className='display-url-wrapper'>
+                {urls.map((url: ShortenedURL, index) => (
+                    <div className='display-box' key={index}>
+                        <ul>
+                            <li><a href={url.original}>{url.original}</a></li>
+                            <hr className='hrule' />
+                            <div className='flex-dir-row display-bundle'>
+                                <li className='display-short'><a href={url.shortened}>{url.shortened}</a></li>
+                                <li><button className='btn input-btn btn-copy' onClick={(e) => copyToClipboard(url.shortened, e)}>Copy</button></li>
+                            </div>
+                        </ul>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
